2d9b991ee4150b4b51b4f44eb3b4babc7d85b10f
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-18 19:42:47 -0700

    Notes/Outline

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+Create a new instance of the Game class and add event listeners for the start button and onscreen keyboard buttons:
+
+    Add a click event listener to the "Start Game" button which creates a new Game object and starts the game by
+    calling the startGame() method.
+
+
+    Add click event listeners to each of the onscreen keyboard buttons, so that clicking a button calls the
+    handleInteraction() method on the Game object. Event delegation can also be used in order to avoid having to add
+    an event listener to each individual keyboard button. Clicking the space between and around the onscreen keyboard
+    buttons should not result in the handleInteraction() method being called.
