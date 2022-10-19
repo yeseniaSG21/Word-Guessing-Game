d822f0388ba0b76fab04156da077f2ce5c0e7a67
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-19 01:33:53 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,26 +9,13 @@ class Phrase {
       * When the player correctly guesses a letter, the empty box is replaced with the matched letter.
     **/
     addPhraseToDisplay() {
+        const ul = document.getElementById('phrase');
 
+        for ( let i = 0; i < this.phrase.length; i++ ) {
+          let placeholder = document.createElement('li');
+        }
     };
 }
-
-<div id="phrase" class="section">
-    <ul>
-        <li class="hide letter h">h</li>
-        <li class="hide letter o">o</li>
-        <li class="hide letter w">w</li>
-        <li class="space"> </li>
-        <li class="hide letter a">a</li>
-        <li class="hide letter r">r</li>
-        <li class="hide letter e">e</li>
-        <li class="space"> </li>
-        <li class="hide letter y">y</li>
-        <li class="hide letter o">o</li>
-        <li class="hide letter u">u</li>
-    </ul>
-</div>
-
 
 /**
 checkLetter(): checks to see if the letter selected by the player matches a letter in the phrase.
