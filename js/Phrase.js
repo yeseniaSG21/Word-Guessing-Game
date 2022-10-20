6e55796387053c4faac5afbc4471fed53d846611
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-20 00:18:33 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,11 +7,11 @@ class Phrase {
       this.phrase = phrase.toLowerCase();
     }
 
-    /**
-      * When the game starts, a letter placeholders is added.
-      * Each letter is presented by an empty box, one li element for each letter.
-      * When the player correctly guesses a letter, the empty box is replaced with the matched letter.
-    **/
+/*****
+  * When the game starts, a letter placeholders is added.
+  * Each letter is presented by an empty box, one li element for each letter.
+  * When the player correctly guesses a letter, the empty box is replaced with the matched letter.
+*****/
     addPhraseToDisplay() {
         const phraseletters = this.phrase.split('');
 
@@ -31,19 +31,19 @@ class Phrase {
         });
     };
 
-    /**
-      * Checks if passed letter matches a letter in the phrase.
-      * @param (string) letter - Letter to check
-      * @return - booleen value
-    */
+/*****
+  * Checks if passed letter matches a letter in the phrase.
+  * @param (string) letter - Letter to check
+  * @return - booleen value
+*****/
     checkLetter(letter) {
         return this.phrase.includes(letter);
     };
 
-    /**
-      * Displays passed letter on screen after a match is found.
-      * @param (string) letter - Letter to display
-    */
+/*****
+  * Displays passed letter on screen after a match is found.
+  * @param (string) letter - Letter to display
+*****/
     showMatchedLetter(letter) {
         const match = document.querySelector('.letter');
 
